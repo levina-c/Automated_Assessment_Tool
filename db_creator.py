8a99eb8405ec01a8578ab7bd1e39277369ab7804
Tsz Wai Levina Chan <chant8@cardiff.ac.uk>
2022-04-07 14:30:45 +0100

    corrected spellings

--- db_creator.py
+++ db_creator.py
@@ -1,15 +1,15 @@
 from aat import db
-from aat.models import Courses, Assessments, Type1Questions, Type2Questions
+from aat.models import Courses, Assessments, AssessmentQuestions, Type1Questions, Type2Questions
 
 db.drop_all()
-db.creat_all()
+db.create_all()
 
-course1 = Course(courseCode = 'CMT120', courseName = 'Fundamentals of Programming')
-course2 = Course(courseCode = 'CMT210', courseName = 'Algorithms, Data Structures and Programming')
-course2 = Course(courseCode = 'CMT220', courseName = 'Databases and Modelling')
+course1 = Courses(courseCode = 'CMT120', courseName = 'Fundamentals of Programming')
+course2 = Courses(courseCode = 'CMT210', courseName = 'Algorithms, Data Structures and Programming')
+course2 = Courses(courseCode = 'CMT220', courseName = 'Databases and Modelling')
 
 t1q1 = Type1Questions(
-    course_id = 'CMT220', 
+    course_code = 'CMT220', 
     content = 'What does SQL stand for?', 
     optA = 'Structured Query Language',
     optB = 'Sequel',
@@ -18,7 +18,7 @@ t1q1 = Type1Questions(
     difficulty = 'Easy',
     used = False )
 t1q2 = Type1Questions(
-    course_id = 'CMT220', 
+    course_code = 'CMT220', 
     content = '_____ is used to denote a relationship in Chen notation.', 
     optA = 'Square',
     optB = 'Circle',
@@ -28,7 +28,7 @@ t1q2 = Type1Questions(
     difficulty = 'Easy',
     used = False )
 t2q1 = Type2Questions(
-    course_id = 'CMT220',
+    course_code = 'CMT220',
     content = 'The Cartesian product of sets A = {1, 2} and B = {3, 4} is {(1, 3), (1, 4), (2, 3), (2, 4)}', 
     answer = 'C',
     explanation = 'The Cartesian product X×Y between two sets X and Y is the set of all possible ordered pairs with first element from X and second element from Y: X×Y={(x,y):x∈X and y∈Y}.',
